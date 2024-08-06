da855d1527a1e581bf0f92a123a280115d524cf6
Cristina Secu <secu.cristina@yahoo.com>
2024-08-05 22:57:01 -0400

    create Redux store and React application

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  /* The useSelector hook creates a subscription to the Redux Store
+  - whenever the store changes, this component that is subscribed to the store will re-render*/
+  const customer = useSelector((store) => store.customer.fullName);
+
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
