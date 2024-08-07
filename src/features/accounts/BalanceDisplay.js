4d3620896b7f9bfd23014ff64de39ad011ff2ede
Cristina Secu <secu.cristina@yahoo.com>
2024-08-07 16:24:05 -0400

    read balance state from REDUX store using legacy way of connecting React and Redux

--- src/features/accounts/BalanceDisplay.js
+++ src/features/accounts/BalanceDisplay.js
@@ -1,4 +1,11 @@
-import { useSelector } from "react-redux";
+import { connect } from "react-redux";
+
+/* 
+useSelector and useDispatch hooks are the modern way of using Redux in React
+Before these hooks existed, developers used the Connect API (this method is still used in older codebases);
+
+The component below uses the Connect API to read data from the Redux store.
+*/
 
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
@@ -7,9 +14,26 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  const accountBalance = useSelector((store) => store.account.balance);
-  return <div className="balance">{formatCurrency(accountBalance)}</div>;
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+/* This function receives the state object from the Redux store */
+function mapStateToProps(state) {
+  /* This function returns an object where we can define the name of a prop that our component 
+  should receive */
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
+
+/* 
+  - connect() function is provided by the React-Redux library;
+  - the connect() function takes in as an argument a function("mapStateToProps" - naming convention) 
+  that in turn will return another function
+  - the returned function will receive a component as an argument
+  - the returned function is basically a component that receives the expected prop ("balance" prop in this example)
+
+ */
